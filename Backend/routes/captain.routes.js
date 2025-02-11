226319f6ff3ffafc2dab179e81bb751abf9f4303
GreatnessGrace <graceunstoppable7@gmail.com>
2025-02-11 16:10:32 +0530

    API: Captain logout

--- Backend/routes/captain.routes.js
+++ Backend/routes/captain.routes.js
@@ -1,4 +1,4 @@
-const captainController = require('../controllers/captain.controller');
+const captainController = require('../controllers/captain.controller.js');
 const express = require('express');
 const router = express.Router();
 const { body } = require("express-validator")
@@ -26,5 +26,7 @@ router.post('/login', [
 )
 
 router.get('/profile', authMiddleware.authCaptain, captainController.getCaptainProfile)
+
+router.get('/logout', authMiddleware.authCaptain, captainController.logoutCaptain)
 
 module.exports = router;
