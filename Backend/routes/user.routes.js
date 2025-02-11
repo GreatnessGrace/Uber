208d6dace8b35c81a000a06cc998128e0896f70a
GreatnessGrace <graceunstoppable7@gmail.com>
2025-02-11 09:34:37 +0530

    API [logout]

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -8,17 +8,18 @@ router.post('/register', [
     body('email').isEmail().withMessage('Email is Invalid'),
     body('fullname.firstname').isLength({ min: 3 }).withMessage('First Nname must be at least 3 charcters long'),
     body('password').isLength({ min: 6}).withMessage('Password must be at least 6 charcters long')
-
-
-],     userController.registerUser
+],
+    userController.registerUser
 )
 
 router.post('/login', [
     body('email').isEmail().withMessage('Email is Invalid'),
     body('password').isLength({ min: 6}).withMessage('Password must be at least 6 charcters long'),
-
-], userController.login)
+],
+    userController.login)
 
 router.get('/profile', authMiddleware.authUser, userController.getUserProfile)
+
+router.get('/logout', authMiddleware.authUser, userController.logoutUser)
 
 module.exports = router;
