7459608c2686b3a5c31de6e656ad72275050520e
GreatnessGrace <graceunstoppable7@gmail.com>
2025-02-12 11:24:47 +0530

    Captains authentictaion pages routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,21 @@
 import React, { useContext } from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Start from './pages/Start'
+import Home from './pages/Home'
 import UserLogin from './pages/UserLogin'
 import UserSignup from './pages/UserSignup'
-import Home from './pages/Home'
-
+import Captainlogin from './pages/Captainlogin'
+import CaptainSignup from './pages/CaptainSignup'
 const App = () => {
-
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Start />} />
+        <Route path='/' element={<Home />} />
         <Route path='/login' element={<UserLogin />} />
-
         <Route path='/signup' element={<UserSignup />} />
-        <Route path='/home' element={<Home />} />
-        
-       
-       
+        <Route path='/captain-login' element={<Captainlogin />} />
+        <Route path='/captain-signup' element={<CaptainSignup />} />
       </Routes>
     </div>
   )
 }
-
 export default App
