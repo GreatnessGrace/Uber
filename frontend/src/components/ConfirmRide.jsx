5af80a3f50942fc7bf673a2dd30101a985f13087
GreatnessGrace <graceunstoppable7@gmail.com>
2025-02-12 17:08:53 +0530

    Confirm Ride Update

--- frontend/src/components/ConfirmRide.jsx
+++ frontend/src/components/ConfirmRide.jsx
@@ -3,7 +3,7 @@ const ConfirmRide = (props) => {
     return (
         <div>
             <h5 className='p-1 text-center w-[93%] absolute top-0' onClick={() => {
-                props.setVehiclePanel(false)
+                props.setConfirmRidePanel(false)
             }}><i className="text-3xl text-gray-200 ri-arrow-down-wide-line"></i></h5>
             <h3 className='text-2xl font-semibold mb-5'>Confirm your Ride</h3>
             <div className='flex gap-2 justify-between flex-col items-center'>
@@ -31,7 +31,10 @@ const ConfirmRide = (props) => {
                         </div>
                     </div>
                 </div>
-                <button className='w-full mt-5 bg-green-600 text-white font-semibold p-2 rounded-lg'>Confirm</button>
+                <button onClick={()=>{
+                props.setVehicleFound(true)
+                props.setConfirmRidePanel(false)
+                }} className='w-full mt-5 bg-green-600 text-white font-semibold p-2 rounded-lg'>Confirm</button>
             </div>
         </div>
     )
