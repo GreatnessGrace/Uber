b2f330e294bef6cecff97c35f0c1db27ce1dc04e
GreatnessGrace <graceunstoppable7@gmail.com>
2025-02-21 10:36:07 +0530

    API: get-coordinates

--- Backend/app.js
+++ Backend/app.js
@@ -7,6 +7,8 @@ const app = express();
 const connectToDb = require('./db/db');
 const userRoutes = require('./routes/user.routes');
 const captainRoutes = require('./routes/captain.routes');
+const rideRoutes = require('./routes/ride.routes');
+const mapsRoutes = require('./routes/maps.routes.js');
 
 connectToDb();
 
@@ -20,5 +22,7 @@ app.get('/', (req, res) => {
 })
 app.use('/users', userRoutes);
 app.use('/captains', captainRoutes);
+app.use('/rides', rideRoutes)
+app.use('/maps', mapsRoutes);
 
 module.exports = app;
