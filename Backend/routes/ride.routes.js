090a3105d5ab6a8e17935903ddeec39a10a7bbe1
GreatnessGrace <graceunstoppable7@gmail.com>
2025-02-22 23:40:06 +0530

    API: confirm ride

--- Backend/routes/ride.routes.js
+++ Backend/routes/ride.routes.js
@@ -1,17 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { body } = require('express-validator');
-const rideController = require('../controllers/ride.controller');
-const authMiddleware = require('../middlewares/auth.middleware');
+const { body } = require("express-validator");
+const rideController = require("../controllers/ride.controller");
+const authMiddleware = require("../middlewares/auth.middleware");
 
-router.post('/create',
-    authMiddleware.authUser,
-    body('pickup').isString().isLength({ min: 3 }).withMessage('Invalid pickup address'),
-    body('destination').isString().isLength({ min: 3 }).withMessage('Invalid destination address'),
-    body('vehicleType').isString().isIn([ 'auto', 'car', 'moto' ]).withMessage('Invalid vehicle type'),
-    rideController.createRide
-)
+router.post(
+  "/create",
+  authMiddleware.authUser,
+  body("pickup")
+    .isString()
+    .isLength({ min: 3 })
+    .withMessage("Invalid pickup address"),
+  body("destination")
+    .isString()
+    .isLength({ min: 3 })
+    .withMessage("Invalid destination address"),
+  body("vehicleType")
+    .isString()
+    .isIn(["auto", "car", "moto"])
+    .withMessage("Invalid vehicle type"),
+  rideController.createRide
+);
+
+router.post(
+  "/confirm",
+  authMiddleware.authCaptain,
+  body("rideId").isMongoId().withMessage("Invalid ride id"),
+  rideController.confirmRide
+);
 
 module.exports = router;
-
-    
--- Backend/services/ride.service.js
+++ Backend/services/ride.service.js
@@ -1,75 +1,114 @@
-const rideModel = require('../models/ride.model');
-const mapService = require('./maps.service');
-const bcrypt = require('bcrypt');
-const crypto = require('crypto');
+const rideModel = require("../models/ride.model");
+const mapService = require("./maps.service");
+const bcrypt = require("bcrypt");
+const crypto = require("crypto");
 
 async function getFare(pickup, destination) {
+  if (!pickup || !destination) {
+    throw new Error("Pickup and destination are required");
+  }
 
-    if (!pickup || !destination) {
-        throw new Error('Pickup and destination are required');
-    }
+  const distanceTime = await mapService.getDistanceTime(pickup, destination);
 
-    const distanceTime = await mapService.getDistanceTime(pickup, destination);
+  const baseFare = {
+    auto: 30,
+    car: 50,
+    moto: 20,
+  };
 
-    const baseFare = {
-        auto: 30,
-        car: 50,
-        moto: 20
-    };
+  const perKmRate = {
+    auto: 10,
+    car: 15,
+    moto: 8,
+  };
 
-    const perKmRate = {
-        auto: 10,
-        car: 15,
-        moto: 8
-    };
+  const perMinuteRate = {
+    auto: 2,
+    car: 3,
+    moto: 1.5,
+  };
 
-    const perMinuteRate = {
-        auto: 2,
-        car: 3,
-        moto: 1.5
-    };
+  console.log(distanceTime);
 
-    console.log(distanceTime);
+  const fare = {
+    auto:
+      baseFare.auto +
+      (distanceTime.distance.value / 1000) * perKmRate.auto +
+      (distanceTime.duration.value / 60) * perMinuteRate.auto,
+    car:
+      baseFare.car +
+      (distanceTime.distance.value / 1000) * perKmRate.car +
+      (distanceTime.duration.value / 60) * perMinuteRate.car,
+    moto:
+      baseFare.moto +
+      (distanceTime.distance.value / 1000) * perKmRate.moto +
+      (distanceTime.duration.value / 60) * perMinuteRate.moto,
+  };
 
-    const fare = {
-        auto: baseFare.auto + ((distanceTime.distance.value / 1000) * perKmRate.auto) + ((distanceTime.duration.value / 60) * perMinuteRate.auto),
-        car: baseFare.car + ((distanceTime.distance.value / 1000) * perKmRate.car) + ((distanceTime.duration.value / 60) * perMinuteRate.car),
-        moto: baseFare.moto + ((distanceTime.distance.value / 1000) * perKmRate.moto) + ((distanceTime.duration.value / 60) * perMinuteRate.moto)
-    };
-
-    return fare;
-
-
+  return fare;
 }
-
 
 function getOtp(num) {
-    function generateOtp(num) {
-        const otp = crypto.randomInt(Math.pow(10, num - 1), Math.pow(10, num)).toString();
-        return otp;
-    }
-    return generateOtp(num);
+  function generateOtp(num) {
+    const otp = crypto
+      .randomInt(Math.pow(10, num - 1), Math.pow(10, num))
+      .toString();
+    return otp;
+  }
+  return generateOtp(num);
 }
-
 
 module.exports.createRide = async ({
-    user, pickup, destination, vehicleType
+  user,
+  pickup,
+  destination,
+  vehicleType,
 }) => {
-    if (!user || !pickup || !destination || !vehicleType) {
-        throw new Error('All fields are required');
+  if (!user || !pickup || !destination || !vehicleType) {
+    throw new Error("All fields are required");
+  }
+
+  const fare = await getFare(pickup, destination);
+
+  console.log(fare);
+
+  const ride = rideModel.create({
+    user,
+    pickup,
+    destination,
+    otp: getOtp(6),
+    fare: fare[vehicleType],
+  });
+
+  return ride;
+};
+
+module.exports.confirmRide = async ({ rideId, captain }) => {
+  if (!rideId) {
+    throw new Error("Ride id is required");
+  }
+
+  await rideModel.findOneAndUpdate(
+    {
+      _id: rideId,
+    },
+    {
+      status: "accepted",
+      captain: captain._id,
     }
+  );
 
-    const fare = await getFare(pickup, destination);
-
-    console.log(fare);
-
-    const ride = rideModel.create({
-        user,
-        pickup,
-        destination,
-        otp: getOtp(6),
-        fare: fare[ vehicleType ]
+  const ride = await rideModel
+    .findOne({
+      _id: rideId,
     })
+    .populate("user")
+    .populate("captain")
+    .select("+otp");
 
-    return ride;
-}
+  if (!ride) {
+    throw new Error("Ride not found");
+  }
+
+  return ride;
+};
