a8992816bda9f59ae39ffb3289c8b7e90466929a
GreatnessGrace <graceunstoppable7@gmail.com>
2025-02-24 12:35:15 +0530

    ride-started event for rider

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -11,6 +11,7 @@ import WaitingForDriver from '../components/WaitingForDriver';
 import { SocketContext } from '../context/SocketContext';
 import { useContext } from 'react';
 import { UserDataContext } from '../context/UserContext';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
     const [ pickup, setPickup ] = useState('')
@@ -32,6 +33,7 @@ const Home = () => {
     const [ fare, setFare ] = useState({})
     const [ vehicleType, setVehicleType ] = useState(null)
     const [ ride, setRide ] = useState(null)
+    const navigate = useNavigate()
 
     const { socket } = useContext(SocketContext)
     const { user } = useContext(UserDataContext)
@@ -46,6 +48,12 @@ const Home = () => {
         setRide(ride)
     })
 
+    socket.on('ride-started', ride => {
+        console.log("ride")
+        setWaitingForDriver(false)
+        navigate('/riding')
+    })
+    
     const handlePickupChange = async (e) => {
         setPickup(e.target.value)
         try {
