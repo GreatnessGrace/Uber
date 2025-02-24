f4068695dd79a2ec33c256071c8e82b1dcbef238
GreatnessGrace <graceunstoppable7@gmail.com>
2025-02-24 12:34:48 +0530

    API: start-ride

--- Backend/routes/ride.routes.js
+++ Backend/routes/ride.routes.js
@@ -29,4 +29,11 @@ router.post(
   rideController.confirmRide
 );
 
+router.get('/start-ride',
+  authMiddleware.authCaptain,
+  query('rideId').isMongoId().withMessage('Invalid ride id'),
+  query('otp').isString().isLength({ min: 6, max: 6 }).withMessage('Invalid OTP'),
+  rideController.startRide
+)
+
 module.exports = router;
